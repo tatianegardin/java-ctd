e2440f6907d8d1420bb34d7bf8ce9f3ff652d136
tatianegardin <contatotatianegardin@hotmail.com>
2022-06-20 23:03:36 -0300

    exercicio consultorio

---
+++ avaliacao-i/src/com/consultorio/models/Consulta.java
@@ -0,0 +1,13 @@
+package com.consultorio.models;
+
+public class Consulta {
+    private String dataConsulta, horarioConsulta, especialidade;
+
+
+    public Consulta(String dataConsulta, String horarioConsulta, String especialidade) {
+        this.dataConsulta = dataConsulta;
+        this.horarioConsulta = horarioConsulta;
+        this.especialidade = especialidade;
+    }
+
+}
---
+++ avaliacao-i/src/com/consultorio/models/Paciente.java
@@ -0,0 +1,32 @@
+package com.consultorio.models;
+
+public abstract class Paciente {
+    private String nome, sobrenome, dataNascimento;
+    private boolean primeiraConsulta;
+
+    public Paciente(String nome, String sobrenome, String dataNascimento) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNascimento = dataNascimento;
+        this.primeiraConsulta = false;
+    }
+
+    public void avaliacaoInicial(){
+        if(!primeiraConsulta) {
+            System.out.println("Preencha a ficha de anamnese!");
+            this.primeiraConsulta = true;
+        }else {
+            System.out.println("Você já é nosso paciente!");
+        }
+    }
+
+    public void agendarConsulta(String data, String horario, String especialidade){
+        avaliacaoInicial();
+        Consulta consulta = new Consulta(data, horario, especialidade);
+        System.out.println("Consulta agendada com sucesso!");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
